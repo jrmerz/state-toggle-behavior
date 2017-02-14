d6cb9c3b04b3578ebaba9f5ed8738a661895fae1
Justin Merz <jrmerz@gmail.com>
2017-02-13 19:40:02 -0800

    allowing additional toggle type

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var StateToggleBehavior = {
   ready : function() {
-    var eles = this.querySelectorAll('[state]');
-    for( var i = 0; i < eles.length; i++ ) {
-      eles[i].style.display = 'none';
+    var states = this.states || [];
+    if( states.indexOf('state') === -1 ) {
+      states.push('state');
     }
+
+    states.forEach((type) => {
+      var eles = this.querySelectorAll('['+type+']');
+      for( var i = 0; i < eles.length; i++ ) {
+        eles[i].style.display = 'none';
+      }
+    });
   },
 
-  toggleState : function(state) {
-    var eles = this.querySelectorAll('[state]');
+  toggleState : function(state, type) {
+    if( !type ) type = 'state';
+
+    var eles = this.querySelectorAll('['+type+']');
     var i, ele;
     for( i = 0; i < eles.length; i++ ) {
       ele = eles[i];
-      ele.style.display = (ele.getAttribute('state') === state) ? 'block' : 'none';
+      ele.style.display = (ele.getAttribute(type) === state) ? 'block' : 'none';
     }
   }
 }
